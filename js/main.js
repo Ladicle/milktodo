4d8ba2edc23cd868f86a5c110075881b2a58db4e
Ladicle <Ladiclexxx@gmail.com>
2015-09-21 17:59:36 +0900

    Arrange style

--- js/main.js
+++ js/main.js
@@ -1,29 +1,64 @@
 (function(global){
-    // milkcocoaへの接続&DataStore設定
-    var milkcocoa = new MilkCocoa("YOUR APPLICATION ID"); // FIXME: 自分のIDに変更
-    var todoDataStore = milkcocoa.dataStore('todos')
+    LOCAL_STORAGE_KEY="MILKTODO_APP_KEY"
 
     // ページ読み込みイベント
     global.onload = onload;
     function onload() {
-	var inputTodoBox = document.getElementById('input_todo_box');
-	var addTodoBtn = document.getElementById('add_todo_btn');
-	var todoList = document.getElementById('todo_list');
+	// todoオブジェクトの取得
+	var inputTodoBox = document.getElementById('input-todo-box');
+	var addTodoBtn = document.getElementById('add-todo-btn');
+	var todoList = document.getElementById('todo-list');
+
+	// milkcocoaへの接続&DataStore設定
+	var milkcocoa = null;
+	var todoDataStore = null;
+	var registAppidBtn = document.getElementById('regist-appid-btn');
+
+	appId = localStorage.getItem(LOCAL_STORAGE_KEY);
+	if ( appId ) {
+	    document.getElementById('input-appid-box').value = appId;
+	    registAppidBtn.click();
+	}
+
+	registAppidBtn.addEventListener('click', function(e) {
+	    appIdBox = document.getElementById('input-appid-box');
+	    parentAppIdBox = appIdBox.parentNode;
+	    appId = appIdBox.value;
+	    console.info("appId: " + appId);
+
+	    if ( !appId ) {
+		parentAppIdBox.className = parentAppIdBox.className + " has-error"
+		return;
+	    }
+
+	    milkcocoa = new MilkCocoa(appId);
+	    todoDataStore = milkcocoa.dataStore('todos');
+	    parentAppIdBox.className = parentAppIdBox.className + " has-success"
+
+	    // dataStoreから取得したtodoを描画する
+	    todoDataStore.stream().sort('desc').size(20).next(function(
+		err, todos) {
+		todos.forEach(function(todo) {
+		    renderTodo(todo.value);
+		});
+	    });
+
+	    // LocalStorageにappidを追加
+	    localStorage.setItem(LOCAL_STORAGE_KEY, appId);
+
+	    // pushイベントを監視
+	    // イベント発生時に追加したtodoを描画
+	    todoDataStore.on('push', function(pushed) {
+		renderTodo(pushed.value);
+	    });
+	});
 
 	// todoの内容をdiv#todoListへ描画
-	function render_todo(todo) {
-	    var todoColumn = document.createElement('div');
+	function renderTodo(todo) {
+	    var todoColumn = document.createElement('li');
 	    todoColumn.textContent = todo.content;
 	    todoList.appendChild(todoColumn);
 	}
-
-	// 読み込み時にdataStoreから取得したtodoを描画する
-	todoDataStore.stream().sort('desc').size(20).next(function(
-	    err, todos) {
-	    todos.forEach(function(todo) {
-		render_todo(todo.value);
-	    });
-	});
 
 	// クリック時にtodoの内容をdataStoreに保存
 	addTodoBtn.addEventListener('click', function(e) {
@@ -36,12 +71,6 @@
 		content : todoContent
 	    });
 	    inputTodoBox.value = "";
-	});
-
-	// pushイベントを監視
-	// イベント発生時に追加したtodoを描画
-	todoDataStore.on('push', function(pushed) {
-	    render_todo(pushed.value);
 	});
     }
 }(window))
